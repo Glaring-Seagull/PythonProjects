0ada4492ec8d6744106bd02f56e7a181a914f93d
Glaring-Seagull <jestdragon@gmail.com>
2022-06-03 09:03:52 -0400

    Added a little loop to make the program repeatable if the user so desires.

--- What should I have.py
+++ What should I have.py
@@ -1,20 +1,23 @@
 import random
-import time
 def answer (answernumber):
     if answernumber == 1:
         return 'Banana'
     elif answernumber == 2:
         return 'Peaches and cream'
     elif answernumber == 3:
-        return 'chocolate'
+        return 'Chocolate'
     elif answernumber == 4:
         return 'Vanilla'
     elif answernumber == 5:
         return 'Strawberry'
-
-r = random.randint(1,5)
-fortune = answer(r)
-print('Oh magical and all mighty PyRoe')
-print('What should I consume for sustenance today?')
-print(fortune)
-time.sleep(5)
+while True:
+    ran = random.randint(1,5)
+    fortune = answer(ran)
+    print('Oh magical and all mighty PyRoe')
+    print('What should I consume for sustenance today?')
+    print(fortune)
+    check = input ("Press 'z' to do another fortune or press any other key to quit.")
+    if check.lower() == "z": #loop back to start
+        continue
+    print("Enjoy your food!")
+    break
