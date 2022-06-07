c17c65239dab8e19377ca6dbdd015034aee9f16d
Glaring-Seagull <jestdragon@gmail.com>
2022-06-07 13:23:42 -0400

    Added comments to prev projects, added project III

--- JSON_Practice/Project II/JSON_Practice_II.py
+++ JSON_Practice/Project II/JSON_Practice_II.py
@@ -1,7 +1,8 @@
 import json
-
+#This project is like the last in this serise but using a slightly more complicated dataset 
 #Like Project I, this will open our json file and load it to the variable json_object
 with open('response.json', 'r') as f:
     json_object = json.loads(f.read())
 
+#This print statement is targeting the "ip_address key in our first dict"
 print(json_object['computer_commands']['computer_command'][0]['computers']['computer']['ip_address'])
